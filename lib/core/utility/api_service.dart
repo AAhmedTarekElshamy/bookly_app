2ca64df03301a7f3b664bcced8cfa1ae2bdf17f6
Ahmed Tarek <ahmedtarik20188@gmail.com>
2024-09-04 12:35:38 +0300

    implement fetchNewestBooks()

--- lib/core/utility/api_service.dart
+++ lib/core/utility/api_service.dart
@@ -2,10 +2,10 @@ import 'package:dio/dio.dart';
 
 class ApiService {
   final String _baseUrl = 'https://www.googleapis.com/books/v1/';
-  final dio = Dio();
+  final _dio = Dio();
 
   Future<Map<String, dynamic>?> get({required String endPoint}) async {
-    var response = await dio.get('$_baseUrl$endPoint');
+    var response = await _dio.get('$_baseUrl$endPoint');
     return response.data;
   }
 }
--- lib/features/home/repositry/home_repo_implement.dart
+++ lib/features/home/repositry/home_repo_implement.dart
@@ -1,18 +1,39 @@
+import 'dart:convert';
+
 import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/features/home/data/models/book_model.dart';
 import 'package:bookly_app/features/home/repositry/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
+import '../../../core/utility/api_service.dart';
+
 class HomeRepoImp extends HomeRepo{
+
+    final ApiService apiService;
+
+  HomeRepoImp({required this.apiService});
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+try{
+  var data= await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest&q=subject=programming');
+  // Assuming `data` is a JSON response and needs to be converted to `BookModel` list
+  List<BookModel> books =  [];
+  for(var item in data?['items']){
+    books.add(BookModel.fromJson(item)) ;
+    return Right(books);
+  }
+}catch(e){
+  return Left(ServerFailure());
+}
+
     throw UnimplementedError();
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
+
+
     throw UnimplementedError();
   }
 
