6ae262d50502678da7794b15cea3f589e3308282
Ahmed Tarek <ahmedtarik20188@gmail.com>
2024-09-04 12:05:54 +0300

    implement api_service.dart

---
+++ lib/core/utility/api_service.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final String _baseUrl = 'https://www.googleapis.com/books/v1/';
+  final dio = Dio();
+
+  Future<Map<String, dynamic>?> get({required String endPoint}) async {
+    var response = await dio.get('$_baseUrl$endPoint');
+    return response.data;
+  }
+}
