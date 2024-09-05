0a04b9ea9413b07905eb122f7bc3624313f64282
Ahmed Tarek <ahmedtarik20188@gmail.com>
2024-09-05 11:36:42 +0300

    implement serverFailure from dio

--- lib/features/home/repositry/home_repo_implement.dart
+++ lib/features/home/repositry/home_repo_implement.dart
@@ -4,6 +4,8 @@ import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/features/home/data/models/book_model.dart';
 import 'package:bookly_app/features/home/repositry/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 
 import '../../../core/utility/api_service.dart';
 
@@ -13,24 +15,37 @@ class HomeRepoImp extends HomeRepo{
 
   HomeRepoImp({required this.apiService});
 
-  @override
-  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
-try{
-  var data= await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest&q=subject=programming');
-  // Assuming `data` is a JSON response and needs to be converted to `BookModel` list
-  List<BookModel> books =  [];
-  for(var item in data?['items']){
-    books.add(BookModel.fromJson(item)) ;
-    return Right(books);
-  }
-}catch(e){
-  return Left(ServerFailure());
-}
 
-    throw UnimplementedError();
-  }
 
-  @override
+    @override
+    Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+      try {
+        // Fetch data from API
+        var data = await apiService.get(
+            endPoint: 'volumes?q=programming&filter=free-ebooks&orderBy=newest'
+        );
+
+        // Initialize list of books
+        List<BookModel> books = [];
+
+        // Parse books from the response
+        for (var item in data?['items']) {
+          books.add(BookModel.fromJson(item));
+        }
+
+        return Right(books);
+      } catch (e) {
+        // Catch and handle server error
+        if (e is DioException ) {
+          return Left(ServerFailure.DioException(e));
+        } else {
+          return Left(ServerFailure(errorMsg: 'Unknown error occurred'));
+        }
+      }
+    }
+
+
+    @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
 
 
